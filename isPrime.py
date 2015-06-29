1a86968dcb77d75862cc2a790ad029cf15c6ede5
martin0107 <martin.gundersen_93@me.com>
2015-06-30 00:04:06 +0200

    Fixed prime-scripts

--- isPrime.py
+++ isPrime.py
@@ -1,11 +1,9 @@
 # Checks if number is a prime
-def isPrime(num):
-    if num < 2:
-        return False
-    if num == 2:
-        return True
-    else:
-        for div in range(2,num):
-            if num % div == 0:
-                return False
-        return True
+def isPrime(number):
+    if number==2 or number==3: return True
+    if number%2==0 or number<2: return False
+    for i in range(3,int(number**0.5)+1,2):
+        if number%i==0:
+            return False    
+    return True
+print isPrime(input())
